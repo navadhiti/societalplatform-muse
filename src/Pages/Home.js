66147e3f4fa981a6109de271720cb138a49733b9
bhanu-nd <bhanu@navadhiti.com>
2022-11-30 10:49:49 +0530

    component floder is updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,22 +1,22 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
-
+import { Container } from "@mui/material";
 
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
-            <h2>home</h2>
-            <h3>Home</h3>
-            <h4>Home</h4>
-            <Typography variant="h1" gutterBottom>
-            Welcome to our
-annual publication
-            </Typography>
-            <Typography variant="h2" gutterBottom>
-            Welcome to our
-annual publication
-            </Typography>
+            <Container fixed>
+                <h1>Home</h1>
+                <h2>home</h2>
+                <h3>Home</h3>
+                <h4>Home</h4>
+                <Typography variant="h1" gutterBottom>
+                    Welcome to our annual publication
+                </Typography>
+                <Typography variant="h2" gutterBottom>
+                    Welcome to our annual publication
+                </Typography>
+            </Container>
         </div>
     );
 };
