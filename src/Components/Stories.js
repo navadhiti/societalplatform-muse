f3e10af203d25dd6119460bd4c5a18e971108e44
rajesh-nd <rajesh@navadhiti.com>
2022-12-01 23:12:08 +0530

    changes in our stories card

--- src/Components/Stories.js
+++ src/Components/Stories.js
@@ -1,191 +1,181 @@
 import {
-    Badge,
-    Box,
-    Button,
-    Card,
-    CardActionArea,
-    CardActions,
-    CardContent,
-    CardMedia,
-    Chip,
-    Container,
-    Grid,
-    IconButton,
-    Stack,
-    Typography,
-} from "@mui/material";
-import React from "react";
-import ShareIcon from "@mui/icons-material/Share";
-import card1 from "../Assets/Images/Card.png";
-import card2 from "../Assets/Images/Card1.png";
-import card3 from "../Assets/Images/Card2.png";
-import Divider from "../Assets/Images/Divider.png";
+  Badge,
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Chip,
+  Container,
+  Grid,
+  IconButton,
+  Stack,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+import ShareIcon from '@mui/icons-material/Share';
+import card1 from '../Assets/Images/Card.png';
+import card2 from '../Assets/Images/Card1.png';
+import card3 from '../Assets/Images/Card2.png';
+import Divider from '../Assets/Images/Divider.png';
 
-import SpeedDial from "@mui/material/SpeedDial";
-import SpeedDialIcon from "@mui/material/SpeedDialIcon";
-import SpeedDialAction from "@mui/material/SpeedDialAction";
-import FileCopyIcon from "@mui/icons-material/FileCopyOutlined";
-import SaveIcon from "@mui/icons-material/Save";
-import PrintIcon from "@mui/icons-material/Print";
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
 import Slider from 'react-slick';
 
 const actions = [
-    { icon: <FileCopyIcon />, name: "Copy" },
-    { icon: <SaveIcon />, name: "Save" },
-    { icon: <PrintIcon />, name: "Print" },
-    { icon: <ShareIcon />, name: "Share" },
+  { icon: <FileCopyIcon />, name: 'Copy' },
+  { icon: <SaveIcon />, name: 'Save' },
+  { icon: <PrintIcon />, name: 'Print' },
+  { icon: <ShareIcon />, name: 'Share' },
 ];
 function SampleNextArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-      <div
-        className={className}
-        style={{
-          ...style,
-          display: 'block',
-          background: 'red',
-          borderRadius: '50%',
-        }}
-        onClick={onClick}
-      />
-    );
-  }
-  const styles = {
-    LandingBackground: {
-     backgroundImage: `linear-gradient(to bottom, #ECE4EF, #ECE4EF)`,
-    }, 
-  };
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: 'block',
+        background: 'red',
+        borderRadius: '50%',
+      }}
+      onClick={onClick}
+    />
+  );
+}
+const styles = {
+  LandingBackground: {
+    backgroundImage: `linear-gradient(to bottom, #ECE4EF, #ECE4EF)`,
+  },
+};
 const Stories = () => {
-    const settings = {
-        infinite: true,
-        slidesToShow: 4,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SampleNextArrow />,
-      };
-    return (
-        <>
-            <Box sx={{textAlign:'center'}}  py={6}
-                         style={styles.LandingBackground}>
-                {/* <Container textAlign="center"> */}
-                    <Typography variant="h4" textAlign="center">
-                        Stories of change
-                    </Typography>
-                    <img src={Divider} alt="img" />
-                    <Grid
-                        container
-                        spacing={0}
-                        justifyContent="center"
+  const settings = {
+    infinite: true,
+    slidesToShow: 4,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SampleNextArrow />,
+    variableWidth: true,
+  };
+  return (
+    <>
+      <Box sx={{ textAlign: 'center' }} py={6} style={styles.LandingBackground}>
+        {/* <Container textAlign="center"> */}
+        <Typography variant="h4" textAlign="center">
+          Stories of change
+        </Typography>
+        <img src={Divider} alt="img" />
+        <Grid container spacing={0} justifyContent="center" alignItems="center">
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={10}
+            lg={10}
+
+            // px={4}
+          >
+            <Slider {...settings}>
+              {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((value) => (
+                <Card
+                  sx={{
+                    m: 2,
+                    p: 2,
+                    maxWidth: 350,
+                    border: '1px solid black',
+                    borderRadius: '20px',
+                  }}
+                  key={value}
+                >
+                  <CardMedia
+                    component="img"
+                    height="240"
+                    image={card1}
+                    alt="green iguana"
+                  />
+                  <CardActionArea>
+                    <CardContent sx={{ justifyContent: 'left' }}>
+                      <Typography gutterBottom variant="body1" component="div">
+                        Edition 01,2022
+                        <Badge variant="tag">HEALTH</Badge>
+                      </Typography>
+                      <Typography variant="h4">All teach,all Learn</Typography>
+
+                      {/* <CardActions> */}
+                      <Stack
+                        direction="row"
+                        justifyContent="space-between"
                         alignItems="center"
-                       
-                    >
-                        <Grid
-                            item
-                            xs={12}
-                            sm={12}
-                            md={10}
-                            lg={10}
-
-                            // px={4}
+                        spacing={2}
+                        p={2}
+                      >
+                        <Button
+                          size="small"
+                          color="primary"
+                          variant="contained"
+                          px={3}
                         >
-                            <Slider {...settings}>
-                            {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((value) => (
-                            <Card sx={{m:2,p:2}} key={value}>
-                                <CardActionArea sx={{}}>
-                                    <CardMedia
-                                        component="img"
-                                        height="140"
-                                        image={card1}
-                                        alt="green iguana"
-                                    />
-                                    <CardContent>
-                                        <Typography
-                                            gutterBottom
-                                            variant="body1"
-                                            component="div"
-                                        >
-                                            Edition 01,2022
-                                            <Badge variant="tag">HEALTH</Badge>
-                                        </Typography>
-                                        <Typography variant="h4">
-                                            All teach,all Learn
-                                        </Typography>
+                          Read
+                        </Button>
 
-                                        {/* <CardActions> */}
-                                        <Stack
-                                            direction="row"
-                                            justifyContent="space-between"
-                                            alignItems="center"
-                                            spacing={2}
-                                            p={2}
-                                        >
-                                            <Button
-                                                size="small"
-                                                color="primary"
-                                                variant="contained"
-                                                px={3}
-                                            >
-                                                Read
-                                            </Button>
-
-                                            <Box
-                                                sx={{
-                                                    transform:
-                                                        "translateZ(0px)",
-                                                    flexGrow: 1,
-                                                }}
-                                            >
-                                                <SpeedDial
-                                                    ariaLabel="SpeedDial basic example"
-                                                    sx={{
-                                                        position: "absolute",
-                                                        bottom: "-2rem",
-                                                        right: "-1rem",
-                                                    }}
-                                                    icon={
-                                                        <ShareIcon
-                                                            sx={{
-                                                                width: "35px",
-                                                                height: "35px",
-                                                                border: "1px solid black",
-                                                                backgroundColor:
-                                                                    "#fff",
-                                                                borderRadius:
-                                                                    "50%",
-                                                                p: 1,
-                                                            }}
-                                                        />
-                                                    }
-                                                >
-                                                    {actions.map((action) => (
-                                                        <SpeedDialAction
-                                                            key={action.name}
-                                                            icon={action.icon}
-                                                            tooltipTitle={
-                                                                action.name
-                                                            }
-                                                        />
-                                                    ))}
-                                                </SpeedDial>
-                                            </Box>
-                                        </Stack>
-                                        {/* </CardActions> */}
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
+                        <Box
+                          sx={{
+                            transform: 'translateZ(0px)',
+                            flexGrow: 1,
+                          }}
+                        >
+                          <SpeedDial
+                            ariaLabel="SpeedDial basic example"
+                            sx={{
+                              position: 'absolute',
+                              bottom: '-2rem',
+                              right: '-1rem',
+                            }}
+                            icon={
+                              <ShareIcon
+                                sx={{
+                                  width: '35px',
+                                  height: '35px',
+                                  border: '1px solid black',
+                                  backgroundColor: '#fff',
+                                  borderRadius: '50%',
+                                  p: 1,
+                                }}
+                              />
+                            }
+                          >
+                            {actions.map((action) => (
+                              <SpeedDialAction
+                                key={action.name}
+                                icon={action.icon}
+                                tooltipTitle={action.name}
+                              />
                             ))}
-                            </Slider>
-                        </Grid>
-                       
-                    </Grid>
-                    <Box my={3}>
-                    <Button variant="contained"  
-                       >READ ARTICLES</Button>
-                    </Box>
-                    
-                {/* </Container> */}
-            </Box>
-        </>
-    );
+                          </SpeedDial>
+                        </Box>
+                      </Stack>
+                      {/* </CardActions> */}
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              ))}
+            </Slider>
+          </Grid>
+        </Grid>
+        <Box my={3}>
+          <Button variant="contained">READ ARTICLES</Button>
+        </Box>
+
+        {/* </Container> */}
+      </Box>
+    </>
+  );
 };
 
 export default Stories;
--- src/Themes/Typography.js
+++ src/Themes/Typography.js
@@ -3,229 +3,229 @@
  * @param {JsonObject} theme theme customization object
  */
 
-import { createTheme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
 
-import "@fontsource/montserrat-alternates";
-import "@fontsource/montserrat";
+import '@fontsource/montserrat-alternates';
+import '@fontsource/montserrat';
 // Create a theme instance.
 const PALETTE = {
-    PRIMARY: "#f8f9fa",
-    SECONDARY: "#935FB0",
-    TERITIARY: "#F5f5f5",
+  PRIMARY: '#f8f9fa',
+  SECONDARY: '#935FB0',
+  TERITIARY: '#F5f5f5',
 };
 const FONT = {
-    FAMILY: {
-        ASSISTANT: "Montserrat Alternates",
-        INTER: "Montserrat ",
-    },
-    SIZES: {
-        12: "0.75rem",
-        14: "0.9rem",
-        16: "1rem",
-        18: "1.125rem",
-        20: "1.25rem",
-        22: "1.375rem",
-        24: "1.5rem",
-        44: "2.75rem",
-        64: "4rem",
-    },
-    WEIGHT: {
-        BOLD: 900,
-        SEMI_BOLD: 700,
-        DEMI_BOLD: 600,
-        MEDIUM: 500,
-        REGULAR: 400,
-        LIGHT: 300,
-    },
+  FAMILY: {
+    ASSISTANT: 'Montserrat Alternates',
+    INTER: 'Montserrat ',
+  },
+  SIZES: {
+    12: '0.75rem',
+    14: '0.9rem',
+    16: '1rem',
+    18: '1.125rem',
+    20: '1.25rem',
+    22: '1.375rem',
+    24: '1.5rem',
+    44: '2.75rem',
+    64: '4rem',
+  },
+  WEIGHT: {
+    BOLD: 900,
+    SEMI_BOLD: 700,
+    DEMI_BOLD: 600,
+    MEDIUM: 500,
+    REGULAR: 400,
+    LIGHT: 300,
+  },
 };
 
 const theme = createTheme({
-    typography: {
-        allVariants: {
-            fontFamily: "Montserrat Alternates",
-        },
-        h1: {
-            fontWeight: FONT.WEIGHT.BOLD,
-            fontSize: FONT.SIZES[44],
-            color: PALETTE.BLACK_SHADE_1,
-        },
-        h2: {
-            fontWeight: FONT.WEIGHT.BOLD,
-            fontSize: FONT.SIZES[44],
-            color: PALETTE.SENARY,
-            marginBottom: "30px",
-        },
-        h3: {
-            fontWeight: FONT.WEIGHT.MEDIUM,
-            fontSize: FONT.SIZES[44],
-            color: PALETTE.SENARY,
-            textTransform: "capitalize",
-        },
-        h4: {
-            fontWeight: FONT.WEIGHT.DEMI_BOLD,
-            fontFamily: "Montserrat",
-            fontSize: FONT.SIZES[24],
-            color: PALETTE.SENARY,
-        },
-        h5: {
-            fontWeight: FONT.WEIGHT.REGULAR,
-            fontSize: FONT.SIZES[22],
-            color: PALETTE.SENARY,
-        },
-        h6: {
-            fontWeight: FONT.WEIGHT.DEMI_BOLD,
-            fontSize: FONT.SIZES[16],
-            fontFamily: FONT.FAMILY.INTER,
-            marginBottom: "10px",
-        },
-        body: {
-            fontWeight: FONT.WEIGHT.DEMI_BOLD,
-            fontSize: FONT.SIZES[14],
-            fontFamily: FONT.FAMILY.INTER,
-        },
+  typography: {
+    allVariants: {
+      fontFamily: 'Montserrat Alternates',
+    },
+    h1: {
+      fontWeight: FONT.WEIGHT.BOLD,
+      fontSize: FONT.SIZES[44],
+      color: PALETTE.BLACK_SHADE_1,
+    },
+    h2: {
+      fontWeight: FONT.WEIGHT.BOLD,
+      fontSize: FONT.SIZES[44],
+      color: PALETTE.SENARY,
+      marginBottom: '30px',
+    },
+    h3: {
+      fontWeight: FONT.WEIGHT.MEDIUM,
+      fontSize: FONT.SIZES[44],
+      color: PALETTE.SENARY,
+      textTransform: 'capitalize',
+    },
+    h4: {
+      fontWeight: FONT.WEIGHT.DEMI_BOLD,
+      fontFamily: 'Montserrat',
+      fontSize: FONT.SIZES[24],
+      color: PALETTE.SENARY,
+    },
+    h5: {
+      fontWeight: FONT.WEIGHT.REGULAR,
+      fontSize: FONT.SIZES[22],
+      color: PALETTE.SENARY,
+    },
+    h6: {
+      fontWeight: FONT.WEIGHT.DEMI_BOLD,
+      fontSize: FONT.SIZES[16],
+      fontFamily: FONT.FAMILY.INTER,
+      marginBottom: '10px',
+    },
+    body: {
+      fontWeight: FONT.WEIGHT.DEMI_BOLD,
+      fontSize: FONT.SIZES[14],
+      fontFamily: FONT.FAMILY.INTER,
+    },
 
-        body1: {
-            fontWeight: FONT.WEIGHT.DEMI_BOLD,
+    body1: {
+      fontWeight: FONT.WEIGHT.DEMI_BOLD,
+      fontSize: FONT.SIZES[12],
+      fontFamily: FONT.FAMILY.INTER,
+    },
+    link: {
+      fontWeight: FONT.WEIGHT.DEMI_BOLD,
+      fontSize: FONT.SIZES[20],
+      lineHeight: '145%',
+      color: '#178290',
+      cursor: 'pointer',
+      textDecoration: 'none',
+    },
+  },
+  palette: {
+    primary: {
+      main: '##fdfdfd08',
+      contrastText: '#000',
+    },
+    secondary: {
+      main: '#485ec4',
+    },
+    color1: {
+      main: '#74788D',
+    },
+  },
+  components: {
+    MuiButton: {
+      variants: [
+        {
+          props: { variant: 'contained' },
+          style: {
+            '&:hover': {
+              boxShadow:
+                '3px 6px 0px -4px rgb(24 12 12 / 99%), 3px 2px 2px 0px rgb(0 0 0 / 98%), 3px 3px 0px 0px rgb(0 0 0 / 95%)',
+            },
+            border: '1px solid black',
+            fontFamily: FONT.FAMILY.INTER,
             fontSize: FONT.SIZES[12],
+            fontWeight: FONT.WEIGHT.SEMI_BOLD,
+
+            borderRadius: '10px',
+          },
+        },
+      ],
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        // props:{classes:"AppBorder"},
+        root: {
+          borderBottom: '1px solid black',
+          paddingBottom: '1rem',
+        },
+      },
+    },
+    MuiCard: {
+      variants: [
+        {
+          props: { classes: 'values' },
+          style: {
+            '&:hover': {
+              boxShadow:
+                '3px 6px 0px -4px rgb(24 12 12 / 99%), 3px 2px 2px 0px rgb(0 0 0 / 98%), 3px 3px 0px 0px rgb(0 0 0 / 95%)',
+            },
+            border: '1px solid black',
             fontFamily: FONT.FAMILY.INTER,
+            fontSize: FONT.SIZES[16],
+            fontWeight: FONT.WEIGHT.SEMI_BOLD,
+            boxShadow:
+              '0px 6px 0px -4px rgb(24 12 12 / 99%), 0px 2px 2px 0px rgb(0 0 0 / 98%), 0px 8px 0px 0px rgb(0 0 0 / 95%)',
+            borderRadius: '10px',
+            minHeight: '12rem',
+            alignItems: 'center',
+            padding: '4.5rem 0',
+            backgroundColor: 'transparent',
+            cursor: 'pointer',
+            // margin:'2.8rem'
+          },
         },
-        link: {
-            fontWeight: FONT.WEIGHT.DEMI_BOLD,
-            fontSize: FONT.SIZES[20],
-            lineHeight: "145%",
-            color: "#178290",
-            cursor: "pointer",
-            textDecoration: "none",
-        },
-    },
-    palette: {
-        primary: {
-            main: "##fdfdfd08",
-            contrastText: "#000",
-        },
-        secondary: {
-            main: "#485ec4",
-        },
-        color1: {
-            main: "#74788D",
-        },
-    },
-    components: {
-        MuiButton: {
-            variants: [
-                {
-                    props: { variant: "contained" },
-                    style: {
-                        "&:hover": {
-                            boxShadow:
-                                "3px 6px 0px -4px rgb(24 12 12 / 99%), 3px 2px 2px 0px rgb(0 0 0 / 98%), 3px 3px 0px 0px rgb(0 0 0 / 95%)",
-                        },
-                        border: "1px solid black",
-                        fontFamily: FONT.FAMILY.INTER,
-                        fontSize: FONT.SIZES[12],
-                        fontWeight: FONT.WEIGHT.SEMI_BOLD,
-
-                        borderRadius: "10px",
-                    },
-                },
-            ],
-        },
-        MuiAppBar: {
-            styleOverrides: {
-                // props:{classes:"AppBorder"},
-                root: {
-                    borderBottom: "1px solid black",
-                    paddingBottom: "1rem",
-                },
+        {
+          props: { classes: 'values1' },
+          style: {
+            '&:hover': {
+              boxShadow:
+                '3px 6px 0px -4px rgb(24 12 12 / 99%), 3px 2px 2px 0px rgb(0 0 0 / 98%), 3px 3px 0px 0px rgb(0 0 0 / 95%)',
             },
+            border: '1px solid black',
+            fontFamily: FONT.FAMILY.INTER,
+            fontSize: FONT.SIZES[14],
+            fontWeight: FONT.WEIGHT.MEDIUM,
+            boxShadow:
+              '0px 6px 0px -4px rgb(24 12 12 / 99%), 0px 2px 2px 0px rgb(0 0 0 / 98%), 0px 8px 0px 0px rgb(0 0 0 / 95%)',
+            borderRadius: '10px',
+            minHeight: '12rem',
+            alignItems: 'center',
+            padding: '1rem',
+            backgroundColor: '#ECE4EF',
+            cursor: 'pointer',
+            // margin:'2.8rem'
+          },
         },
-        MuiCard: {
-            variants: [
-                {
-                    props: { classes: "values" },
-                    style: {
-                        "&:hover": {
-                            boxShadow:
-                                "3px 6px 0px -4px rgb(24 12 12 / 99%), 3px 2px 2px 0px rgb(0 0 0 / 98%), 3px 3px 0px 0px rgb(0 0 0 / 95%)",
-                        },
-                        border: "1px solid black",
-                        fontFamily: FONT.FAMILY.INTER,
-                        fontSize: FONT.SIZES[16],
-                        fontWeight: FONT.WEIGHT.SEMI_BOLD,
-                        boxShadow:
-                            "0px 6px 0px -4px rgb(24 12 12 / 99%), 0px 2px 2px 0px rgb(0 0 0 / 98%), 0px 8px 0px 0px rgb(0 0 0 / 95%)",
-                        borderRadius: "10px",
-                        minHeight: "12rem",
-                        alignItems: "center",
-                        padding: "4.5rem 0",
-                        backgroundColor: "transparent",
-                        cursor:'pointer',
-                        // margin:'2.8rem'
-                    },
-                },
-                {
-                    props: { classes: "values1" },
-                    style: {
-                        "&:hover": {
-                            boxShadow:
-                                "3px 6px 0px -4px rgb(24 12 12 / 99%), 3px 2px 2px 0px rgb(0 0 0 / 98%), 3px 3px 0px 0px rgb(0 0 0 / 95%)",
-                        },
-                        border: "1px solid black",
-                        fontFamily: FONT.FAMILY.INTER,
-                        fontSize: FONT.SIZES[14],
-                        fontWeight: FONT.WEIGHT.MEDIUM,
-                        boxShadow:
-                            "0px 6px 0px -4px rgb(24 12 12 / 99%), 0px 2px 2px 0px rgb(0 0 0 / 98%), 0px 8px 0px 0px rgb(0 0 0 / 95%)",
-                        borderRadius: "10px",
-                        minHeight: "12rem",
-                        alignItems: "center",
-                        padding: "1rem",
-                        backgroundColor: "#ECE4EF",
-                        cursor:'pointer',
-                        // margin:'2.8rem'
-                    },
-                },
-            ],
-        },
-        MuiBadge: {
-            variants: [
-                {
-                    props: { variant: "tag" },
-                    style: {
-                        fontFamily: FONT.FAMILY.INTER,
-                        fontSize: FONT.SIZES[10],
-                        fontWeight: FONT.WEIGHT.MEDIUM,
-                        backgroundColor: PALETTE.PRIMARY,
-                        padding: "0.3rem",
-                        margin: "0 0.5rem",
-                    },
-                },
-            ],
-        },
-        MuiSpeedDial: {
-            styleOverrides: {
-                // props:{classes:"AppBorder"},
-                root: {
-                    backgroundColor: "transpent",
-                    boxShadow: "none",
-                    //    border:'1px solid',
-                },
-            },
-        },
-
-        // MuiIconButton: {
-        //     styleOverrides: {
-        //         // props:{classes:"AppBorder"},
-        //         root: {
-        //             border: "1px solid black",
-        //             borderRadius: "50%",
-        //             "&:hover": {
-        //                 boxShadow:
-        //                     "3px 6px 0px -4px rgb(24 12 12 / 99%), 3px 2px 2px 0px rgb(0 0 0 / 98%), 3px 3px 0px 0px rgb(0 0 0 / 95%)",
-        //             },
-        //         },
-        //     },
-        // },
+      ],
     },
+    MuiBadge: {
+      variants: [
+        {
+          props: { variant: 'tag' },
+          style: {
+            fontFamily: FONT.FAMILY.INTER,
+            fontSize: FONT.SIZES[10],
+            fontWeight: FONT.WEIGHT.MEDIUM,
+            backgroundColor: PALETTE.PRIMARY,
+            padding: '0.3rem',
+            margin: '0 0.5rem',
+          },
+        },
+      ],
+    },
+    MuiSpeedDial: {
+      styleOverrides: {
+        // props:{classes:"AppBorder"},
+        root: {
+          backgroundColor: 'transpent',
+          boxShadow: 'none',
+          //    border:'1px solid',
+        },
+      },
+    },
+
+    // MuiIconButton: {
+    //     styleOverrides: {
+    //         // props:{classes:"AppBorder"},
+    //         root: {
+    //             border: "1px solid black",
+    //             borderRadius: "50%",
+    //             "&:hover": {
+    //                 boxShadow:
+    //                     "3px 6px 0px -4px rgb(24 12 12 / 99%), 3px 2px 2px 0px rgb(0 0 0 / 98%), 3px 3px 0px 0px rgb(0 0 0 / 95%)",
+    //             },
+    //         },
+    //     },
+    // },
+  },
 });
 export default theme;
