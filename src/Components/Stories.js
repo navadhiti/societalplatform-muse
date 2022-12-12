f17454d8909b6c3808ba527aae2f30ea8b274a52
rajesh-nd <rajesh@navadhiti.com>
2022-12-12 12:08:25 +0530

    commiting to take pull

---
+++ src/Components/Stories.js
@@ -0,0 +1,192 @@
+import {
+  Badge,
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Chip,
+  Container,
+  Grid,
+  IconButton,
+  Stack,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+import ShareIcon from '@mui/icons-material/Share';
+import card1 from '../Assets/Images/Card.png';
+import card2 from '../Assets/Images/Card1.png';
+import card3 from '../Assets/Images/Card2.png';
+import Divider from '../Assets/Images/Divider.png';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import Slider from 'react-slick';
+import DataFromDB from '../db.json';
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  FacebookShareCount,
+  FacebookIcon,
+} from 'react-share';
+
+console.log(DataFromDB);
+
+const facebookURL = 'https://www.navadhiti.com/';
+
+const actions = [
+  {
+    icon: (
+      <FacebookShareButton url={facebookURL}>
+        <FacebookIcon round={true} />
+      </FacebookShareButton>
+    ),
+    name: 'Facebook',
+  },
+  { icon: <SaveIcon />, name: 'Save' },
+  { icon: <PrintIcon />, name: 'Print' },
+  { icon: <ShareIcon />, name: 'Share' },
+];
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: 'block',
+        background: 'red',
+        borderRadius: '50%',
+      }}
+      onClick={onClick}
+    />
+  );
+}
+const styles = {
+  LandingBackground: {
+    backgroundImage: `linear-gradient(to bottom, #ECE4EF, #ECE4EF)`,
+  },
+};
+const Stories = () => {
+  const settings = {
+    infinite: true,
+    slidesToShow: 4,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SampleNextArrow />,
+    variableWidth: true,
+  };
+  return (
+    <>
+      <Box sx={{ textAlign: 'center' }} py={6} style={styles.LandingBackground}>
+        {/* <Container textAlign="center"> */}
+        <Typography variant="h4" textAlign="center">
+          Stories of change
+        </Typography>
+        <img src={Divider} alt="img" />
+        <Grid container spacing={0} justifyContent="center" alignItems="center">
+          <Grid
+            item
+            xs={12}
+            sm={12}
+            md={10}
+            lg={10}
+            // px={4}
+          >
+            <Slider {...settings}>
+              {DataFromDB.map((data) => (
+                <Card
+                  sx={{
+                    m: 2,
+                    p: 2,
+                    maxWidth: 350,
+                    border: '1px solid black',
+                    borderRadius: '20px',
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    height="240"
+                    image={card1}
+                    alt="green iguana"
+                  />
+                  {/* <CardActionArea> */}
+                  <CardContent sx={{ justifyContent: 'left' }}>
+                    <Typography gutterBottom variant="body1" component="div">
+                      Edition {new Date(data.date).toLocaleDateString('en-CA')}
+                      <Badge variant="tag">HEALTH</Badge>
+                    </Typography>
+                    <Typography variant="h4">All teach,all Learn</Typography>
+                    {/* <CardActions> */}
+                    <Stack
+                      direction="row"
+                      justifyContent="space-between"
+                      alignItems="center"
+                      spacing={2}
+                      p={2}
+                    >
+                      <Button
+                        size="small"
+                        color="primary"
+                        variant="contained"
+                        px={3}
+                      >
+                        Read
+                      </Button>
+                      <Box
+                        sx={{
+                          transform: 'translateZ(0px)',
+                          flexGrow: 1,
+                        }}
+                      >
+                        <SpeedDial
+                          ariaLabel="SpeedDial basic example"
+                          sx={{
+                            position: 'absolute',
+                            bottom: '-2rem',
+                            right: '-1rem',
+                          }}
+                          icon={
+                            <ShareIcon
+                              sx={{
+                                width: '35px',
+                                height: '35px',
+                                border: '1px solid black',
+                                backgroundColor: '#fff',
+                                borderRadius: '50%',
+                                p: 1,
+                              }}
+                            />
+                          }
+                        >
+                          {actions.map((action) => (
+                            <SpeedDialAction
+                              key={action.name}
+                              icon={action.icon}
+                              tooltipTitle={action.name}
+                            />
+                          ))}
+                        </SpeedDial>
+                      </Box>
+                    </Stack>
+                    {/* </CardActions> */}
+                  </CardContent>
+                  {/* </CardActionArea> */}
+                </Card>
+              ))}
+            </Slider>
+          </Grid>
+        </Grid>
+        <Box my={3}>
+          <Button variant="contained">READ ARTICLES</Button>
+        </Box>
+        {/* </Container> */}
+      </Box>
+    </>
+  );
+};
+export default Stories;
--- src/Layout/Layout .js
+++ src/Layout/Layout .js
@@ -1,27 +1,28 @@
-import { Container } from "@mui/system";
-import React from "react";
-import { useNavigate, Route, Routes } from "react-router-dom";
+import { Container } from '@mui/system';
+import React from 'react';
+import { useNavigate, Route, Routes } from 'react-router-dom';
 // import Footer from "../Components/Footer";
 // import Header from "../Components/Header";
-import Home from "../Pages/Home";
+import Home from '../Pages/Home';
+import Stories from '../Components/Stories';
 
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "../Themes/Typography";
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../Themes/Typography';
 const Layout = () => {
-    const navigate = useNavigate();
-    return (
-        <>
-            <ThemeProvider theme={theme}>
-                {/* <Container> */}
-                    {/* <Header /> */}
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                    </Routes>
-                    {/* <Footer /> */}
-                {/* </Container> */}
-           
-            </ThemeProvider>
-        </>
-    );
+  const navigate = useNavigate();
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        {/* <Container> */}
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Stories" element={<Stories />} />
+        </Routes>
+        {/* <Footer /> */}
+        {/* </Container> */}
+      </ThemeProvider>
+    </>
+  );
 };
 export default Layout;
