d768b983244892366f52ae917a5e120e08d63309
rajesh-nd <rajesh@navadhiti.com>
2022-12-21 11:28:50 +0530

    used styled for styling from MUI

---
+++ src/Themes/StyledComponent.js
@@ -0,0 +1,23 @@
+import { styled } from '@mui/system';
+import { Button } from '@mui/material';
+
+import FONT from '../Themes/Font';
+import PALETTE from '../Themes/Palette';
+
+import Scribble from '../Assets/Images/scribble.png';
+
+export const StickyButton = styled(Button)`
+  border: none;
+  background: none;
+
+  &:hover {
+    background: ${`url(${Scribble}) bottom no-repeat`};
+  }
+
+  h2 {
+    font-family: ${FONT.FAMILY.ASSISTANT};
+    font-weight: ${FONT.WEIGHT.REGULAR};
+    font-size: 18px;
+    color: ${PALETTE.SENARY};
+  }
+`;
